b3f86a3c6a7faf35213068ef8d8cd0dd679006c6
andrewcarlsammut <asam0032@um.edu.mt>
2014-02-28 09:25:24 +0100

    This is a new commit

--- funds2.c
+++ funds2.c
@@ -22,7 +22,7 @@ double sum(const funds*);
 
 int main(int argc, const char * argv[])
 {
-
+    printf("Changing to test");
     funds Mario = { "Valletta Fund", 2000, "Mosta Fund", 3000};
     printf("This is the sum of the funds %f", sum(&Mario));
     return 0;
